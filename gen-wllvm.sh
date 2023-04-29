666846b7bda9a1dafa2bee2a1b643f0ef07eb38f
Mingjie Shen <shen497@purdue.edu>
2023-04-29 18:13:18 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-export PATH=`realpath ../gcc-arm-none-eabi-8-2018-q4-major/bin`:$PATH
 make clean && make -C libopencm3 clean
-genbc ../bitcode-db/Amazfitbip -c "make -j$(nproc)"
+
+TOOLCHAIN_BIN=$(realpath $RTOSExploration/toolchain/gcc-arm-none-eabi-8-2018-q4-major/bin)
+export PATH=$TOOLCHAIN_BIN:$PATH
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export BINUTILS_TARGET_PREFIX=$TOOLCHAIN_BIN/arm-none-eabi
+make -j$(nproc) CC=wllvm
+extract-bc bin/main.elf
